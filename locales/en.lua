9eddbc35b3147374538f1646063c015e93b1f7c6
RockySouthpaw <55056068+RockySouthpaw@users.noreply.github.com>
2020-09-28 12:00:07 -0400

    Organized files, added en and es local

---
+++ locales/en.lua
@@ -0,0 +1,12 @@
+Locales['en'] = {
+['msg1'] = "Press ~g~[E]~s~ to talk to the client.",
+['msg2'] = "Client",
+['msg3'] = "I have a job for you, theres a vehicle on your gps, bring it to me quickly",
+['msg4'] = "Find the vehicle and take it to the client.",
+['msg5'] = "Press ~g~[E]~s~ to deliver the vehicle",
+['msg6'] = "Vehicle",
+['msg7'] = "Delivery point",
+['msg8'] = "You are not in the vehicle I asked for!",
+['msg9'] = "You have delivered the vehicle and you were paid ~r~$",
+['msg10'] = "Vehicle theft",
+}
